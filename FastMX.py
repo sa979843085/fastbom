ea7fab791bfbac4e72d8305b9671cc1b1afcc127
sas317 <979843085@qq.com>
2024-12-05 13:57:14 +0800

    增加到排序

--- FastMX.py
+++ FastMX.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import re
 
 # 定义导入BOM数据的函数
 
@@ -14,8 +15,8 @@ def import_bom_data(file_path):
     return bom_data
 
 # 文件路径为D:\万合光电\WHJ82蒸发波导诊断系统\BOM.xlsx
-# bom_data = import_bom_data('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/总BOM.xlsx')
-bom_data = import_bom_data('D:/万合光电/WHJ82蒸发波导诊断系统/BOM.xlsx')
+bom_data = import_bom_data('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/总BOM.xlsx')
+# bom_data = import_bom_data('D:/万合光电/WHJ82蒸发波导诊断系统/BOM.xlsx')
 if bom_data is not None:
     # # 将BOM数据前几行打印出来
     # print(bom_data.head())  
@@ -46,7 +47,9 @@ bom_data = bom_data.reset_index(drop=True)
 
 # 4. 新建列存储“父件的名称”、“父件的数量”、“总数量”
 bom_data['父件的名称'] = ''
+bom_data['父件的代号'] = ''
 bom_data['父件的数量'] = ''
+bom_data['总数量'] = ''
 
 
 
@@ -73,10 +76,12 @@ for index, row in bom_data.iterrows():
     level = row['阶层']
     if level == '0':
         bom_data.at[index, '父件的名称'] = ''
+        bom_data.at[index, '父件的代号'] = ''
         bom_data.at[index, '父件的数量'] = ''
     elif '.' not in level:
         # 如果没有点，父件是阶层为0的零件
         bom_data.at[index, '父件的名称'] = bom_data.loc[bom_data['阶层'] == '0', '零件名称'].values[0]
+        bom_data.at[index, '父件的代号'] = bom_data.loc[bom_data['阶层'] == '0', '零件代号'].values[0]
         bom_data.at[index, '父件的数量'] = bom_data.loc[bom_data['阶层'] == '0', '数量'].values[0]
     else:
         # 如果存在点，去除最后一个点及后面的部分，剩下的字符串就是父件的阶层
@@ -89,28 +94,87 @@ for index, row in bom_data.iterrows():
         else:
             bom_data.at[index, '父件的名称'] = ''
         
+        matches_code = bom_data.loc[bom_data['阶层'] == parent_level, '零件代号']
+        if not matches_code.empty:
+            bom_data.at[index, '父件的代号'] = matches_code.values[0]
+        else:
+            bom_data.at[index, '父件的代号'] = ''
+        
         matches_quantity = bom_data.loc[bom_data['阶层'] == parent_level, '数量']
         if not matches_quantity.empty:
             bom_data.at[index, '父件的数量'] = matches_quantity.values[0]
         else:
             bom_data.at[index, '父件的数量'] = ''
 
+# 根据数量乘以父件的数量更改总数量
+
+bom_data['数量'] = pd.to_numeric(bom_data['数量'], errors='coerce')
+bom_data['父件的数量'] = pd.to_numeric(bom_data['父件的数量'], errors='coerce')
+
+# 可以选择填充 NaN 值，例如用 0 填充
+bom_data['数量'].fillna(0, inplace=True)
+bom_data['父件的数量'].fillna(0, inplace=True)
+
+bom_data['总数量'] = bom_data['数量'].astype(int) * bom_data['父件的数量'].astype(int) 
+
+
+# 将父件代号转换为字符串
+bom_data['父件的代号'] = bom_data['父件的代号'].astype(str)
+
+# 删除没有父件代号的行
+bom_data = bom_data[bom_data['父件的代号'] != 'nan']
+bom_data = bom_data.reset_index(drop=True)
+
+# 创建"分类"列
+bom_data['分类'] = ''
+
+# 将"零件代号"列转换为字符串格式
+bom_data['零件代号'] = bom_data['零件代号'].astype(str)
+# # 打印bom_data的零件代号列前几行
+# print(bom_data['零件代号'].head())
+# 定义分类函数
+def classify_part(part_number):
+    if re.match(r'WH.*0000$', part_number):
+        return '0成品'
+    if re.match(r'WH.*000$', part_number):
+        return '1组件'
+    elif re.match(r'WH.*00$', part_number):
+        return '2分组件'
+    elif re.match(r'WH.*0$', part_number):
+        return '3部件'
+    elif re.match(r'WH.*[^-].*$', part_number):
+        return '4分部件'
+    elif re.match(r'WH.*-.*$', part_number):
+        return '5零件'
+    elif 'GB' in part_number:
+        return '6标准件'
+    elif part_number == 'nan':
+        return '7外购件'
+    else:
+        return '8未分类'
+
+# 遍历数据框，为"分类"列赋值
+for index, row in bom_data.iterrows():
+    part_number = row['零件代号']
+    bom_data.at[index, '分类'] = classify_part(part_number)
+
+# 根据分类进行pair排序,顺序未成品>组件>分组件>部件>分部件>标准件>外购件
+bom_data['分类'] = bom_data['分类'].astype(str)
+
+bom_data = bom_data.sort_values(['分类', '零件代号','父件的代号'], ascending=[True, True, True])
+
+# # 如果存在重复的零件, 则只保留一条记录
+# bom_data = bom_data.drop_duplicates(['零件代号'], keep='first')
+
 
 
 
 if bom_data is not None:
     # 在同目录下生成新的excel
-    # bom_data.to_excel('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
-    bom_data.to_excel('D:/万合光电/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
+    bom_data.to_excel('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
+    # bom_data.to_excel('D:/万合光电/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
     print("BOM数据修改成功")
 else:
     print("BOM数据修改失败")
-
-
-
-
-
-
-
 
 
