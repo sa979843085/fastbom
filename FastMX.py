5ab6bd89f38e1ee50c67899dbaa8ad6970979a92
sas317 <979843085@qq.com>
2024-12-06 16:56:07 +0800

    基本完成

--- FastMX.py
+++ FastMX.py
@@ -1,6 +1,26 @@
 import pandas as pd
-import os
 import re
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename(
+    title="选择文件",  # 对话框标题
+    filetypes=(("Excel files", "*.xlsx"), ("CSV files", "*.csv"), ("All files", "*.*"))  # 文件类型过滤
+)
+# 检查用户是否选择了文件
+if file_path:
+    folder_path = os.path.dirname(file_path)
+    print(f"用户选择的文件夹路径: {folder_path}")
+    print(f"用户选择的文件路径: {file_path}")
+    # 在这里可以添加你的代码来处理选择的文件
+else:
+    print("用户取消了文件选择")
+# 关闭Tkinter根窗口
+root.destroy()
 
 # 定义导入BOM数据的函数
 
@@ -14,8 +34,8 @@ def import_bom_data(file_path):
         return None
     return bom_data
 
-# 文件路径为D:\万合光电\WHJ82蒸发波导诊断系统\BOM.xlsx
-bom_data = import_bom_data('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/总BOM.xlsx')
+bom_data = import_bom_data(file_path)
+# bom_data = import_bom_data('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/总BOM.xlsx')
 # bom_data = import_bom_data('D:/万合光电/WHJ82蒸发波导诊断系统/BOM.xlsx')
 if bom_data is not None:
     # # 将BOM数据前几行打印出来
@@ -118,11 +138,18 @@ bom_data['父件的数量'] = pd.to_numeric(bom_data['父件的数量'], errors=
 bom_data['数量'] = bom_data['数量'].fillna(0)
 bom_data['父件的数量'] = bom_data['父件的数量'].fillna(0)
 
-# 将“数量_格式化”填充为“数量”×“父件的数量”的文字
-bom_data['数量_格式化'] = bom_data['数量'].astype(str) + '×' + bom_data['父件的数量'].astype(int).astype(str)
 
 # bom_data['总数量'] = ''
 bom_data['总数量'] = bom_data['数量'].astype(int) * bom_data['父件的数量'].astype(int) 
+
+# 如果总数量大于1，将“数量_格式化”填充为“数量”×“父件的数量”的文字，如果总数量小于等于1，则填充为“总数量”
+def format_quantity(row):
+    if row['总数量'] > 1:
+        return f"{row['数量']}×{int(row['父件的数量'])}"
+    else:
+        return str(row['总数量'])
+
+bom_data['数量_格式化'] = bom_data.apply(format_quantity, axis=1)
 
 
 # 将父件代号转换为字符串
@@ -168,13 +195,15 @@ for index, row in bom_data.iterrows():
     part_number = row['零件代号']
     bom_data.at[index, '分类'] = classify_part(part_number)
 
-# 根据分类进行pair排序,顺序未成品>组件>分组件>部件>分部件>标准件>外购件
+# 根据分类进行pair排序,顺序：成品>组件>分组件>部件>分部件>标准件>外购件
 bom_data['分类'] = bom_data['分类'].astype(str)
 
-bom_data = bom_data.sort_values(['分类', '文件名称', '零件名称'], ascending=[True, True, True])
+bom_data = bom_data.sort_values(['分类', '零件代号','零件名称'], ascending=[True, True, True]) # 按分类和零件代号升序排序
 
+bom_data = bom_data.drop(columns=['序号','父件的名称', '父件的数量'])
 
-bom_data = bom_data.drop(columns=['序号','父件的名称', '父件的数量','分类'])
+# # 去除分类列里面的数字
+# bom_data['分类'] = bom_data['分类'].str.replace(r'\d+', '', regex=True)
 
 # 重新设置索引
 bom_data = bom_data.reset_index(drop=True)
@@ -194,10 +223,15 @@ for col in bom_data.columns:
     bom_data[col] = bom_data[col].astype(str)
 
 
+
+##################
+# 增加汇总行
+##################
 # 根据文件名称分组
 grouped = bom_data.groupby('文件名称')
 # 处理大于一行的组
 new_bom_data = pd.DataFrame()  # 创建一个新的 DataFrame 用于存储修改后的数据
+
 
 for name, group in grouped:
     if len(group) > 1:
@@ -206,13 +240,15 @@ for name, group in grouped:
         total_mass = group['总重(kg)'].astype(float).sum().astype(str)
         new_row = group.iloc[0].copy()
         new_row['总数量'] = total_quantity
-        new_row['重量(kg)'] = ''
+        new_row['单重(kg)'] = ''
         new_row['总重(kg)'] = total_mass
+        new_row['父件的代号'] = ''
+        new_row['数量_格式化']= ''
         # 汇总行的'排序辅助列'去掉.后面的部分
         new_row['排序辅助列'] = new_row['排序辅助列'].split('.')[0]
         # 将组内行的零件名称、零件代号、父件的代号、总数量、总重(kg)，然后设置为空
 
-        group.loc[:, ['零件名称', '零件代号', '父件代号', '总数量', '总重(kg)','备注']] = ''
+        group.loc[:, ['零件名称', '零件代号', '总数量', '总重(kg)','备注']] = ''
         
         # 将修改后的组数据添加到新的 DataFrame 中
         new_bom_data = pd.concat([new_bom_data, group], ignore_index=True)
@@ -223,7 +259,7 @@ for name, group in grouped:
         # 如果组内只有一个元素，直接添加到新的 DataFrame 中
         new_bom_data = pd.concat([new_bom_data, group], ignore_index=True)
 
-# 最后，new_bom_data 包含了所有修改后的数据
+# 覆盖bom_data
 bom_data = new_bom_data
 
 # 排序辅助列转换为浮点数
@@ -239,9 +275,31 @@ bom_data.drop('排序辅助列', axis=1, inplace=True)
 # 清空所有字符串为nan的单元格
 bom_data = bom_data.replace('nan', '')
 # 将第一行的数量列内容赋值给第一行的父件数量列
-bom_data.loc[0, '父件的数量'] = bom_data.loc[0, '数量']
+bom_data.loc[0, '总数量'] = bom_data.loc[0, '数量']
 # 清空第一行的数量_格式化列
 bom_data.loc[0, '数量_格式化'] = ''
+
+
+# 定义一个函数，用于在每个分类前增加一行
+def add_category_row(group):
+    # 获取分组的名称（即'分类'列的值）
+    category = group.name
+    # 如果分类不是“成品”，则添加新行
+    if category != '0成品':
+        category_row = pd.DataFrame([[''] * len(bom_data.columns)], columns=bom_data.columns)
+        # 将category中的数字去除
+        category = re.sub(r'\d+', '', category)
+        category_row['零件名称'] = category
+        return pd.concat([category_row, group], ignore_index=True)
+    else:
+        return group
+
+
+
+# 使用groupby并应用add_category_row函数
+bom_data = bom_data.groupby('分类', group_keys=False).apply(add_category_row, include_groups=False).reset_index(drop=True)
+
+
 
 # 删除阶层列、文件名称、数量列
 bom_data.drop('阶层', axis=1, inplace=True)
@@ -250,10 +308,16 @@ bom_data.drop('数量', axis=1, inplace=True)
 
 
 
+# 重新整理列的顺序
+new_order = ['零件代号', '零件名称', '父件的代号', '数量_格式化', '总数量', '单重(kg)', '总重(kg)', '材料', '备注']
+bom_data = bom_data[new_order]
+
+
 
 if bom_data is not None:
-    # 在同目录下生成新的excel
-    bom_data.to_excel('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
+    # 在folder_path目录下生成新的excel
+    bom_data.to_excel(os.path.join(folder_path, 'BOM数据修改.xlsx'), index=False)
+    # bom_data.to_excel('E:/万合结构/1项目/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
     # bom_data.to_excel('D:/万合光电/WHJ82蒸发波导诊断系统/BOM数据修改.xlsx', index=False)
     print("BOM数据修改成功")
 else:
